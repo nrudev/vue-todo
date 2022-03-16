9059ee93a765c208ad3a07799c1746feffbe9aec
HEESEUNG KOO <nrudev812@gmail.com>
2022-03-16 15:31:05 +0900

    Input 기본 기능 구현 및 스타일링

--- src/App.vue
+++ src/App.vue
@@ -28,5 +28,9 @@ body {
   background-color: blanchedalmond;
   text-align: center;
   color: #001432;
+  font-family: 'Ubuntu', sans-serif;
+}
+.shadow {
+  box-shadow: 5px 10px 10px rgba(0, 0, 0, 0.03);
 }
 </style>
--- src/components/TodoInput.vue
+++ src/components/TodoInput.vue
@@ -1,9 +1,61 @@
 <template>
-  <div>Input</div>
+  <div class="inputBox shadow">
+    <input type="text" v-model="newTodo" @keyup.enter="addTodo" />
+    <span class="addContainer" @click="addTodo">
+      <i class="fa-solid fa-plus addBtn"></i>
+    </span>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      newTodo: '',
+      keyNum: localStorage.length,
+    };
+  },
+  methods: {
+    addTodo() {
+      if (this.newTodo !== '') {
+        localStorage.setItem(this.keyNum++, this.newTodo);
+      }
+      this.clearInput();
+    },
+    clearInput() {
+      this.newTodo = '';
+    },
+  },
+};
 </script>
 
-<style></style>
+<style scoped>
+input:focus {
+  outline: none;
+}
+.inputBox {
+  background-color: white;
+  height: 50px;
+  line-height: 50px;
+  border-radius: 5px;
+}
+.inputBox input {
+  height: 40px;
+  width: 80%;
+  border-style: none;
+  font-size: 0.9rem;
+}
+.addContainer {
+  float: right;
+  background: linear-gradient(to right, #0099a4, #00498c);
+  display: block;
+  width: 3rem;
+  border-radius: 0 5px 5px 0;
+  cursor: pointer;
+}
+.addBtn {
+  color: white;
+  vertical-align: middle;
+  font-size: 30px;
+}
+</style>
